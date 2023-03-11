5322ca1bdefff75b253d862ae2b724324e9c1628
Vitalik1788 <tarasenkovitalik2013@ukr.net>
2023-03-11 15:03:14 +0200

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
 
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+let timerId = null;
+
+refs.stopBtn.setAttribute('disabled', true);
+
+function onStartBtnClick(event) {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);  
+  refs.startBtn.setAttribute('disabled', true);
+  refs.stopBtn.removeAttribute('disabled', true);
+  
+};
+
+function onStopBtnClick(event) {
+  clearInterval(timerId);
+  refs.startBtn.removeAttribute('disabled', true);
+  refs.stopBtn.setAttribute('disabled', true);
+
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
