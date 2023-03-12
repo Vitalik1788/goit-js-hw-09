3dbadbbd560f1f319b06e85d78d98c4e914f93a3
Vitalik1788 <tarasenkovitalik2013@ukr.net>
2023-03-12 15:18:23 +0200

    update

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -11,8 +11,12 @@ const refs = {
   seconds: document.querySelector('span[data-seconds]'),
 };
 
+let userSelectedDate = null;
+let userDefaultDate = null;
+let intervalId = null;
+
+
 refs.startBtn.setAttribute('disabled', true);
-refs.startBtn.addEventListener('click', onBtnStartClick);
 
 const options = {
   enableTime: true,
@@ -20,51 +24,76 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    onUserDateValue(selectedDates);
+    if (selectedDates[0].getTime() <= options.defaultDate.getTime()) {
+      Notiflix.Notify.failure('Please choose a date in the future');
+      refs.startBtn.disabled = true;
+      return;
+    } else {
+      refs.startBtn.disabled = false;
+      const setTimer = () => {
+        userSelectedDate = selectedDates[0].getTime();
+        timer.start();
+      }
+      refs.startBtn.addEventListener('click', setTimer);      
+    }
   }
 };
 flatpickr('#datetime-picker', options);
 
+const timer = {
+  start() {
+    intervalId = setInterval(() => {
+      userDefaultDate = Date.now();
+      const deltaTime = userSelectedDate - userDefaultDate;
 
-function onUserDateValue(selectedDates) {
-  const userSelectedDate = selectedDates[0].getTime();
-  const userDefaultDate = options.defaultDate.getTime();
-  if (userSelectedDate <= userDefaultDate) {
-    Notiflix.Notify.warning('Please choose a date in the future');
-    refs.startBtn.setAttribute('disabled', true);
-    return;
-  }
-  refs.startBtn.removeAttribute('disabled', true);
-  const ms = userSelectedDate - userDefaultDate;
-  convertMs(ms);
+      if (deltaTime <= 0) {
+        this.stop();
+        Notiflix.Report.success('Дія лічильника скінчилась', 'Оберіть нову дату та час', 'ОК');
+        return;
+      }
+
+      refs.startBtn.disabled = true;
+      const { days, hours, minutes, seconds } = convertMs(deltaTime);
+      refs.days.textContent = days;
+      refs.hours.textContent = hours;
+      refs.minutes.textContent = minutes;
+      refs.seconds.textContent = seconds;
+    }, 1000);
+  },
+
+  stop() {
+    clearInterval(intervalId);
+    refs.startBtn.disabled = true;    
+    intervalId = null;
+  },    
 };
 
-function onBtnStartClick({ days, hours, minutes, seconds }) {
-  console.log(
-    `залишилось часу до розпродажу: ${days} : ${hours} : ${minutes} : ${seconds}`
-  );
-
+function addLeadingZero(value) {
+    return String(value).padStart(2, '0');
 };
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
 
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+    // Remaining days
+    const days = addLeadingZero(Math.floor(ms / day));
+    // Remaining hours
+    const hours = addLeadingZero(Math.floor((ms % day) / hour));
+    // Remaining minutes
+    const minutes = addLeadingZero(
+      Math.floor(((ms % day) % hour) / minute)
+    );
+    // Remaining seconds
+    const seconds = addLeadingZero(
+      Math.floor((((ms % day) % hour) % minute) / second)
+    );
 
-  onBtnStartClick({ days, hours, minutes, seconds });
-
-  return { days, hours, minutes, seconds };
+    return { days, hours, minutes, seconds };
 };
+
 
 
